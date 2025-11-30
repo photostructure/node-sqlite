faabe83936bced72bfd3d525e434d120a6966cbd
Matthew McEachen <matthew@photostructure.com>
2025-11-30 15:09:08 -0800

    refactor(env-inl.h): remove unused Environment class and clean up comments for clarity

--- src/shims/env-inl.h
+++ src/shims/env-inl.h
@@ -1,19 +1,10 @@
 #ifndef SRC_SHIMS_ENV_INL_H_
 #define SRC_SHIMS_ENV_INL_H_
 
-#include <napi.h>
-
-namespace node {
-
-// Environment stubs
-class Environment {
-public:
-  static Environment *GetCurrent(Napi::Env env) {
-    static Environment instance;
-    return &instance;
-  }
-};
-
-} // namespace node
+// This header exists for Node.js source compatibility.
+// src/upstream/node_sqlite.cc includes "env-inl.h", and we can't modify
+// upstream files (they're auto-synced from Node.js). The actual Environment
+// implementation is in util.h.
+#include "util.h"
 
 #endif // SRC_SHIMS_ENV_INL_H_
